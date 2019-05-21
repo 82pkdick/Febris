eb443e6fd11485bf6e26c370ce0038a9bd4b7df8
82pkdick <82pkdick@gmail.com>
2019-05-21 23:35:10 +0900

    Changed layouts. New side menu.

---
+++ assets/js/test.js
@@ -0,0 +1,26 @@
+;(function(global) {
+
+// ===== Main namespace =====
+if (typeof(global.$site) == 'undefined') {
+  global.$site = {};
+  global.$site.config = {};
+}
+
+global.$site.config = {
+  baseurl: "/Febris",
+  url:     "http://localhost:4000",
+  port:     4000,
+  host:    "127.0.0.1",
+  
+  // custom configurations
+  title:   "Febris",
+  description: "My Description. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  author_name: "82pkdick",
+  author_email: "82pkdick@gmail.com",
+  author_twitter: "82pkdick",
+  author_github: "82pkdick",
+};
+
+})(window);
+
+console.dir(window.$site.config);
